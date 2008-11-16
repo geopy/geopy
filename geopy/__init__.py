ac9763d98af41efcedb35e8da1d3ad74acbe209c
exogen <exogen@535ce8a4-231d-0410-ac7f-15f3cd728b52>
2008-11-16 23:13:53 +0000

    Setting up logging basicConfig
    git-svn-id: https://geopy.googlecode.com/svn/trunk@52 535ce8a4-231d-0410-ac7f-15f3cd728b52

--- geopy/__init__.py
+++ geopy/__init__.py
@@ -1,3 +1,9 @@
+import logging
 from geopy.point import Point
 from geopy.location import Location
 from geopy import geocoders
+
+logging.basicConfig(
+    format="[%(name)s:%(levelname)s] %(message)s",
+    level=logging.DEBUG
+)
