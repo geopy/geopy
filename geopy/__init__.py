93543d2241126079dcf6cfba94cbda489d0e8dbf
exogen <exogen@535ce8a4-231d-0410-ac7f-15f3cd728b52>
2008-11-17 04:26:55 +0000

    removed logging basicConfig. bad idea.
    git-svn-id: https://geopy.googlecode.com/svn/trunk@60 535ce8a4-231d-0410-ac7f-15f3cd728b52

--- geopy/__init__.py
+++ geopy/__init__.py
@@ -1,9 +1,3 @@
-import logging
 from geopy.point import Point
 from geopy.location import Location
 from geopy import geocoders
-
-logging.basicConfig(
-    format="[%(name)s:%(levelname)s] %(message)s",
-    level=logging.DEBUG
-)
