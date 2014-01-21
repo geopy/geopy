ff3a44ac39447751e8daf24d5cec3eb5cdfd0be5
crccheck <c@crccheck.com>
2014-01-20 23:03:33 -0600

    add test coverage to 'components' param

--- test/test_backends.py
+++ test/test_backends.py
@@ -202,6 +202,22 @@ class GoogleV3TestCase(_BackendTestCase): # pylint: disable=R0904,C0111
     def setUp(self):
         self.geocoder = GoogleV3(scheme='http')
 
+    def test_geocode_components(self):
+        known_addr = 'santa cruz'
+        # providing conflicting components should return no results
+        result = self.geocoder.geocode(known_addr, components={
+            'administrative_area': 'CA',
+            'country': 'FR',
+        })
+        self.assertIsNone(result)
+
+        known_coords = (28.4636296, -16.2518467)
+        addr, coords = self.geocoder.geocode(known_addr, components={
+            'country': 'ES',
+        })
+        self.assertAlmostEqual(coords[0], known_coords[0], delta=self.delta_exact)
+        self.assertAlmostEqual(coords[1], known_coords[1], delta=self.delta_exact)
+
     def test_reverse(self):
         known_addr = '1060-1078 Avenue of the Americas, New York, NY 10018, USA'
         known_coords = (40.75376406311989, -73.98489005863667)
