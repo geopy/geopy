1f2052158a483668febe83c67e6e2bb4913fdc4e
basement-labs <bebum@o2.pl>
2014-12-28 22:02:33 +0100

    add navidata geocoder
    add navidata geocoder
    
    minor bugfixes
    
    add navidata geocoder
    
    add navidata geocoder
    
    add navidata geocoder
    
    add navidata geocoder

---
+++ test/geocoders/navidata.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+import unittest
+
+from geopy.compat import u
+from geopy.geocoders import NaviData
+from test.geocoders.util import GeocoderTestBase, env
+
+
+
+class NaviDataTestCase(GeocoderTestBase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.delta = 0.04
+
+    def test_unicode_name(self):
+        """
+        NaviData.geocode unicode
+        """
+
+        self.geocoder = NaviData()
+        self.geocode_run(
+            {"query": "Warszawa, mazowieckie"},
+            {"latitude": 52.231, "longitude": 21.006},
+        )
+
+
+    def test_reverse(self):
+        """
+        NaviData.reverse
+        """
+
+        self.geocoder = NaviData()
+        self.reverse_run(
+            {"query": "51.036963, 16.755802"},
+            {"latitude": 51.036963, "longitude": 16.755802},
+        )
