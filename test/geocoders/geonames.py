5afdb7aa0914e583a71e51dcaefd4c5f8713633e
Sebastian Neubauer <sebineubauer@gmail.com>
2015-04-08 22:45:04 +0100

    added customizable user agent to geonames

--- test/geocoders/geonames.py
+++ test/geocoders/geonames.py
@@ -6,6 +6,16 @@ from geopy.geocoders import GeoNames
 from test.geocoders.util import GeocoderTestBase, env
 
 
+class GeoNamesTestCaseUnitTest(GeocoderTestBase):
+
+    def test_user_agent_custom(self):
+        geocoder = GeoNames(
+            username='DUMMYUSER_NORBERT',
+            user_agent='my_user_agent/1.0'
+        )
+        self.assertEqual(geocoder.headers['User-Agent'], 'my_user_agent/1.0')
+
+
 @unittest.skipUnless(  # pylint: disable=R0904,C0111
     bool(env.get('GEONAMES_USERNAME')),
     "No GEONAMES_USERNAME env variable set"
