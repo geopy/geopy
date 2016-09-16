056f68667c8123fff88a7bb9c16c6e80ca91d01f
Afonso Queiros <afonso.queiros@deliveryhero.com>
2016-09-16 14:32:48 +0200

    adding client_id and secret_key support

--- geopy/geocoders/googleplaces.py
+++ geopy/geocoders/googleplaces.py
@@ -1,10 +1,14 @@
 """
 :class:`.GooglePlaces` is the Google Places API.
 """
+import base64
+import hashlib
+import hmac
 from geopy.compat import urlencode
 from geopy.geocoders.base import Geocoder, DEFAULT_TIMEOUT, DEFAULT_SCHEME
 from geopy.exc import (
     GeocoderQuotaExceeded,
+    ConfigurationError,
     GeocoderQueryError,
 )
 from geopy.location import Location
@@ -23,9 +27,12 @@ class GooglePlaces(Geocoder):  # pylint: disable=R0902
             self,
             api_key=None,
             scheme=DEFAULT_SCHEME,
+            client_id=None,
+            secret_key=None,
             timeout=DEFAULT_TIMEOUT,
             proxies=None,
-            user_agent=None
+            user_agent=None,
+            channel=''
     ):  # pylint: disable=R0913
 
         """
@@ -50,16 +57,54 @@ class GooglePlaces(Geocoder):  # pylint: disable=R0902
             through the specified proxy. E.g., {"https": "192.0.2.0"}. For
             more information, see documentation on
             :class:`urllib2.ProxyHandler`.
+
+            .. versionadded:: 1.12.1
+
+        :param string client_id: If using premier, the account client id.
+
+        :param string secret_key: If using premier, the account secret key.
+
+        :param string channel: If using premier, the channel identifier.
         """
         super(GooglePlaces, self).__init__(
             scheme=scheme, timeout=timeout, proxies=proxies, user_agent=user_agent
         )
+        if client_id and not secret_key:
+            raise ConfigurationError('Must provide secret_key with client_id.')
+        if secret_key and not client_id:
+            raise ConfigurationError('Must provide client_id with secret_key.')
+
+        self.premier = bool(client_id and secret_key)
+        self.client_id = client_id
+        self.secret_key = secret_key
+        self.channel = channel
 
         self.api_key = api_key
-        domain = 'maps.googleapis.com'
+        self.domain = 'maps.googleapis.com'
 
-        self.autocomplete_api = '{scheme}://{domain}/maps/api/place/autocomplete/json'.format(scheme=self.scheme, domain=domain)
-        self.details_api = '{scheme}://{domain}/maps/api/place/details/json'.format(scheme=self.scheme, domain=domain)
+        self.autocomplete_api = '{scheme}://{domain}/maps/api/place/autocomplete/json'.format(scheme=self.scheme, domain=self.domain)
+        self.details_api = '{scheme}://{domain}/maps/api/place/details/json'.format(scheme=self.scheme, domain=self.domain)
+
+
+    def _get_signed_url(self, endpoint, params):
+        """
+        Returns a Premier account signed url. Docs on signature:
+            https://developers.google.com/maps/documentation/business/webservices/auth#digital_signatures
+        """
+        params['client'] = self.client_id
+
+        if self.channel:
+            params['channel'] = self.channel
+
+        path = "{}?{}".format(endpoint, urlencode(params))
+        signature = hmac.new(
+            base64.urlsafe_b64decode(self.secret_key),
+            path.encode('utf-8'),
+            hashlib.sha1
+        )
+        signature = base64.urlsafe_b64encode(signature.digest()).decode('utf-8')
+        return '{}://{}{}&signature={}'.format(self.scheme, self.domain, path, signature)
+
 
     @staticmethod
     def _format_components_param(components):
@@ -136,7 +181,12 @@ class GooglePlaces(Geocoder):  # pylint: disable=R0902
         detail_params['placeid'] = placeid
         if language:
             detail_params['language'] = language
-        details_url = "{}?{}".format(self.details_api, urlencode(detail_params))
+
+        if self.premier is False:
+            details_url = "{}?{}".format(self.details_api, urlencode(detail_params))
+        else:
+            details_url = self._get_signed_url('/maps/api/details/json', detail_params)
+
         detail_result = self.parse_details(self._call_geocoder(details_url))
         formatted_address = detail_result['formatted_address']
         latitude = detail_result['geometry']['location']['lat']
@@ -179,7 +229,11 @@ class GooglePlaces(Geocoder):  # pylint: disable=R0902
         if components is not None:
             autocomplete_params['components'] = self._format_components_param(components)
 
-        autocomplete_url = "{}?{}".format(self.autocomplete_api, urlencode(autocomplete_params))
+        if self.premier is False:
+            autocomplete_url = "{}?{}".format(self.autocomplete_api, urlencode(autocomplete_params))
+        else:
+            autocomplete_url = self._get_signed_url('/maps/api/autocomplete/json', autocomplete_params)
+
         return self.parse_autocomplete(self._call_geocoder(autocomplete_url, timeout=timeout))
 
     def parse_autocomplete(self, autocomplete_page):
--- test/geocoders/googleplaces.py
+++ test/geocoders/googleplaces.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import base64
 from geopy.compat import u
 from geopy.point import Point
 from geopy.geocoders import GooglePlaces
 from test.geocoders.util import GeocoderTestBase, env
+from geopy.compat import u, urlparse, parse_qs
 
 class GooglePlacesTestCase(GeocoderTestBase):  # pylint: disable=R0904,C0111
 
@@ -48,3 +50,21 @@ class GooglePlacesTestCase(GeocoderTestBase):  # pylint: disable=R0904,C0111
                 if "country" in type:
                     country = component['long_name']
                     self.assertEqual(country.encode('utf-8'),self.known_country_ar)
+
+
+    def test_get_signed_url_with_channel(self):
+        """
+        GooglePlaces._get_signed_url
+        """
+        geocoder = GooglePlaces(
+            client_id='my_client_id',
+            secret_key=base64.urlsafe_b64encode('my_secret_key'.encode('utf8')),
+            channel='my_channel'
+        )
+
+        signed_url = geocoder._get_signed_url('/maps/api/autocomplete/json',{'address': '1 5th Ave New York, NY'})
+        params = parse_qs(urlparse(signed_url).query)
+
+        self.assertTrue('channel' in params)
+        self.assertTrue('signature' in params)
+        self.assertTrue('client' in params)
