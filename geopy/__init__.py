233d7394e30978e293dc313d1604f595beb82c1d
Kostya Esmukov <kostya@esmukov.ru>
2018-03-11 14:30:36 +0300

    Pull up python versions (add 3.6, change mentions in docs)

--- geopy/__init__.py
+++ geopy/__init__.py
@@ -4,8 +4,6 @@ geopy is a Python 2 and 3 client for several popular geocoding web services.
 geopy makes it easy for Python developers to locate the coordinates of
 addresses, cities, countries, and landmarks across the globe using third-party
 geocoders and other data sources.
-
-geopy is tested against CPython 2.7, CPython 3.2, CPython 3.4, PyPy, and PyPy3.
 """
 
 from geopy.point import Point
