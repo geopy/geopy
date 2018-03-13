e192fab4c8e72ab647f333387f8b36466802c32c
Kostya Esmukov <kostya@esmukov.ru>
2018-03-13 08:44:36 +0300

    Docs: return back the list of supported Python versions
    When I removed this I assumed that it's ok if it will be missing
    in the docs, but now I realize that it's probably not.

--- geopy/__init__.py
+++ geopy/__init__.py
@@ -4,6 +4,9 @@ geopy is a Python 2 and 3 client for several popular geocoding web services.
 geopy makes it easy for Python developers to locate the coordinates of
 addresses, cities, countries, and landmarks across the globe using third-party
 geocoders and other data sources.
+
+geopy is tested against CPython (versions 2.7, 3.4, 3.5, 3.6), PyPy, and
+PyPy3. geopy does not and will not support CPython 2.6.
 """
 
 from geopy.point import Point
