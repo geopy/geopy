7a9826b2201856a8478160f5882f5be1a172196b
Kostya Esmukov <kostya@esmukov.ru>
2018-03-25 15:56:14 +0300

    Point: add more tests

--- test/test_point.py
+++ test/test_point.py
@@ -7,7 +7,8 @@ import unittest
 from geopy.compat import u
 from geopy.point import Point
 
-class PointTestCase(unittest.TestCase): # pylint: disable=R0904
+
+class PointTestCase(unittest.TestCase):
     """
     geopy.point.Point
     """
@@ -34,6 +35,7 @@ class PointTestCase(unittest.TestCase): # pylint: disable=R0904
             point = Point(each % (self.lat, self.lon))
             self.assertEqual(point.longitude, self.lon)
             self.assertEqual(point.latitude, self.lat)
+            self.assertEqual(point.altitude, 0)
 
     def test_point_str_deg(self):
         """
@@ -51,12 +53,58 @@ class PointTestCase(unittest.TestCase): # pylint: disable=R0904
         point = Point("51 19m 12.9s N, 0 1m 24.95s E")
         self.assertEqual(point.format(), "51 19m 12.9s N, 0 1m 24.95s E")
 
+        point = Point("51 19m 12.9s N, -1 1m 24.95s E, 15000m")
+        self.assertEqual(point.format(), "51 19m 12.9s N, 1 1m 24.95s W, 15.0km")
+
+        # TODO
+        # point = Point("51 19m 12.9s N, -0 1m 24.95s E")
+        # self.assertEqual(point.format(), "51 19m 12.9s N, 0 1m 24.95s W")
+
+        # TODO
+        # with self.assertRaises(ValueError):
+        #     # Z is not a valid direction
+        #     Point("51 19m 12.9s Z, 0 1m 24.95s E")
+
+        with self.assertRaises(ValueError):
+            Point("gibberish")
+
     def test_point_format_altitude(self):
         """
         Point.format() includes altitude
         """
         point = Point(latitude=41.5, longitude=81.0, altitude=2.5)
         self.assertEqual(point.format(), "41 30m 0s N, 81 0m 0s E, 2.5km")
+        self.assertEqual(point.format_decimal(), "41.5, 81.0, 2.5km")
+        self.assertEqual(point.format_decimal('m'), "41.5, 81.0, 2500.0m")
+
+        point = Point(latitude=41.5, longitude=81.0)
+        self.assertEqual(point.format_decimal(), "41.5, 81.0")
+        self.assertEqual(point.format_decimal('m'), "41.5, 81.0, 0.0m")
+
+    def test_point_from_iterable(self):
+        self.assertEqual(Point(1, 2, 3), Point([1, 2, 3]))
+        self.assertEqual(Point(1, 2, 0), Point([1, 2]))
+        self.assertEqual(Point(1, 0, 0), Point([1]))
+        self.assertEqual(Point(0, 0, 0), Point([]))
+
+    def test_point_from_point(self):
+        point = Point(self.lat, self.lon, self.alt)
+        point_copy = Point(point)
+        self.assertTrue(point is not point_copy)
+        self.assertEqual(tuple(point), tuple(point_copy))
+
+    def test_point_degrees_are_normalized(self):
+        point = Point(95, 185, 375)
+        self.assertEqual((-85, -175, 375), tuple(point))
+        point = Point(-95, -185, 375)
+        self.assertEqual((85, 175, 375), tuple(point))
+
+    def test_unpacking(self):
+        point = Point(self.lat, self.lon, self.alt)
+        lat, lon, alt = point
+        self.assertEqual(lat, self.lat)
+        self.assertEqual(lon, self.lon)
+        self.assertEqual(alt, self.alt)
 
     def test_point_getitem(self):
         """
@@ -96,5 +144,22 @@ class PointTestCase(unittest.TestCase): # pylint: disable=R0904
             Point(self.lat+10, self.lon-10, self.alt)
         )
 
+    def test_point_comparison_respects_lists(self):
+        point = Point(self.lat, self.lon, self.alt)
+        l_eq = [self.lat, self.lon, self.alt]
+        l_ne = [self.lat + 1, self.lon, self.alt]
+        self.assertTrue(l_eq == point)
+        self.assertTrue(point == l_eq)
+        self.assertFalse(l_eq != point)
+        self.assertFalse(point != l_eq)
 
+        self.assertFalse(l_ne == point)
+        self.assertFalse(point == l_ne)
+        self.assertTrue(l_ne != point)
+        self.assertTrue(point != l_ne)
 
+    def test_point_comparison_ignores_strings(self):
+        point = Point("1", "2", "3")
+        self.assertFalse(point == "123")
+        self.assertTrue(point != "123")
+        self.assertTrue(point == (1, 2, 3))
