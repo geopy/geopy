geopy/geopy

commit 3479b0c79cff7a68b12b3cf1a5d15e529d2fa114

Point: fix _items vs attrs inconsistency

geopy/point.py
@@ -46,6 +46,9 @@ POINT_PATTERN = re.compile(r"""
 }, re.X)
 
 
+POINT_VALUES_ORDER = ('latitude', 'longitude', 'altitude')
+
+
 class Point(object):
     """
     A geodetic point with latitude, longitude, and altitude.
@@ -100,7 +103,7 @@ class Point(object):
 
     """
 
-    __slots__ = ("latitude", "longitude", "altitude", "_items")
+    __slots__ = ("latitude", "longitude", "altitude")
 
     POINT_PATTERN = POINT_PATTERN
 
@@ -143,20 +146,19 @@ class Point(object):
         self.latitude = latitude
         self.longitude = longitude
         self.altitude = altitude
-        self._items = [self.latitude, self.longitude, self.altitude]
         return self
 
     def __getitem__(self, index):
-        return self._items[index]
+        return getattr(self, POINT_VALUES_ORDER[index])
 
     def __setitem__(self, index, value):
-        self._items[index] = value
+        setattr(self, POINT_VALUES_ORDER[index], value)
 
     def __iter__(self):
         return iter((self.latitude, self.longitude, self.altitude))
 
     def __repr__(self):
-        return "Point(%r, %r, %r)" % tuple(self._items)
+        return "Point(%r, %r, %r)" % tuple(self)
 
     def format(self, altitude=None, deg_char='', min_char='m', sec_char='s'):
         """

test/test_point.py
@@ -125,9 +125,30 @@ class PointTestCase(unittest.TestCase):
         point = Point(self.lat + 10, self.lon + 10, self.alt + 10)
         for each in (0, 1, 2):
             point[each] = point[each] - 10
+
         self.assertEqual(point[0], self.lat)
         self.assertEqual(point[1], self.lon)
         self.assertEqual(point[2], self.alt)
+
+        self.assertEqual(self.coords, tuple(point))
+        self.assertEqual(point.latitude, self.lat)
+        self.assertEqual(point.longitude, self.lon)
+        self.assertEqual(point.altitude, self.alt)
+
+    def test_point_assign_coordinates(self):
+        point = Point(self.lat + 10, self.lon + 10, self.alt + 10)
+        point.latitude = self.lat
+        point.longitude = self.lon
+        point.altitude = self.alt
+
+        self.assertEqual(point[0], self.lat)
+        self.assertEqual(point[1], self.lon)
+        self.assertEqual(point[2], self.alt)
+
+        self.assertEqual(self.coords, tuple(point))
+        self.assertEqual(point.latitude, self.lat)
+        self.assertEqual(point.longitude, self.lon)
+        self.assertEqual(point.altitude, self.alt)
 
     def test_point_eq(self):
         """

commit 0c9b7e0f59825325b38a78af0356a8a8c4165532

Point: support slices (but return tuple instead of a list)
Fixes regression introduced by 3479b0c

geopy/point.py
@@ -46,9 +46,6 @@ POINT_PATTERN = re.compile(r"""
 }, re.X)
 
 
-POINT_VALUES_ORDER = ('latitude', 'longitude', 'altitude')
-
-
 class Point(object):
     """
     A geodetic point with latitude, longitude, and altitude.
@@ -149,10 +146,12 @@ class Point(object):
         return self
 
     def __getitem__(self, index):
-        return getattr(self, POINT_VALUES_ORDER[index])
+        return tuple(self)[index]  # tuple handles slices
 
     def __setitem__(self, index, value):
-        setattr(self, POINT_VALUES_ORDER[index], value)
+        point = list(self)
+        point[index] = value  # list handles slices
+        self.latitude, self.longitude, self.altitude = point
 
     def __iter__(self):
         return iter((self.latitude, self.longitude, self.altitude))
@@ -161,8 +160,7 @@ class Point(object):
         return tuple(self)
 
     def __setstate__(self, state):
-        for idx, value in enumerate(state):
-            self[idx] = value
+        self.latitude, self.longitude, self.altitude = state
 
     def __repr__(self):
         return "Point(%r, %r, %r)" % tuple(self)

test/test_point.py
@@ -119,6 +119,24 @@ class PointTestCase(unittest.TestCase):
         self.assertEqual(point[1], self.lon)
         self.assertEqual(point[2], self.alt)
 
+    def test_point_slices(self):
+        point = Point(self.lat, self.lon, self.alt)
+        self.assertEqual((self.lat, self.lon), point[:2])
+
+        self.assertEqual(self.coords, point[:10])
+        self.assertEqual(self.coords, point[:])
+        self.assertEqual(self.coords[::-1], point[::-1])
+
+        with self.assertRaises(IndexError):
+            point[10]
+
+        with self.assertRaises(TypeError):
+            point[None]
+
+        point[0:2] = (self.lat + 10, self.lon + 10)
+        self.assertEqual((self.lat + 10, self.lon + 10, self.alt),
+                         tuple(point))
+
     def test_point_setitem(self):
         """
         Point.__setitem__