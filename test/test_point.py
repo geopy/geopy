f728960c2d801285a7b12364dfd91bba27df8bae
Filatov Pavel <triksrimer@gmail.com>
2018-04-05 23:00:52 +0300

    added tests for normalization of points (90*c, 180*c, 375) where c is 2...10

--- test/test_point.py
+++ test/test_point.py
@@ -112,6 +112,27 @@ class PointTestCase(unittest.TestCase):
         self.assertEqual((0.0, 0.0, 375.0), tuple(point))  # note that the zeros might be negative
         # ensure that negative zeros are normalized to the positive ones
         self.assertEqual((1.0, 1.0, 1.0), tuple(math.copysign(1.0, x) for x in point))
+        point = Point(90, 180, 375)
+        self.assertEqual((90, 180, 375), tuple(point))
+        point = Point(-90, -180, 375)
+        self.assertEqual((-90, -180, 375), tuple(point))
+        point = Point(-270, -540, 375)
+        self.assertEqual((-90, -180, 375), tuple(point))
+        initial = (90, 180)
+        for counter in range(2, 10):
+            point = Point(*(ini * counter for ini in initial), 375)
+            if counter % 2 == 0:
+                self.assertEqual((0.0, 0.0, 375.0), tuple(point))
+            else:
+                self.assertEqual((-90.0, -180.0, 375.0), tuple(point))
+        initial = (-90, -180)
+        for counter in range(2, 10):
+            point = Point(*(ini * counter for ini in initial), 375)
+            if counter % 2 == 0:
+                self.assertEqual((0.0, 0.0, 375.0), tuple(point))
+            else:
+                self.assertEqual((-90.0, -180.0, 375.0), tuple(point))
+
 
     def test_unpacking(self):
         point = Point(self.lat, self.lon, self.alt)
