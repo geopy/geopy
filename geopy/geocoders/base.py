0516ce02adb853f62e0a3b0362174526f34ba4c6
Kostya Esmukov <kostya@esmukov.ru>
2019-03-24 13:40:36 +0300

    Base Geocoder: remove support for `raw=True` requests
    They aren't used by the current geocoders.

--- geopy/geocoders/base.py
+++ geopy/geocoders/base.py
@@ -267,7 +267,6 @@ class Geocoder:
             url,
             *,
             timeout=DEFAULT_SENTINEL,
-            raw=False,
             deserializer=json.loads,
             headers=None
     ):
@@ -326,9 +325,6 @@ class Geocoder:
             status_code = None
         if status_code in ERROR_CODE_MAP:
             raise ERROR_CODE_MAP[page.status_code]("\n%s" % decode_page(page))
-
-        if raw:
-            return page
 
         page = decode_page(page)
 
