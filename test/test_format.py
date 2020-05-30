28a7d46a09348a5eaf7b0de43d826fcc9d4bcd2e
Kostya Esmukov <kostya@esmukov.ru>
2020-05-31 02:02:35 +0300

    test_format: replace `skip` with `xfail`
    ```
    E   AssertionError: '-13 18\' 60"' != '-13 19\' 0.0"'
    E   - -13 18' 60"
    E   ?      ^  ^
    E   + -13 19' 0.0"
    E   ?      ^  ^^
    ```

--- test/test_format.py
+++ test/test_format.py
@@ -1,18 +1,12 @@
-
-import unittest
+import pytest
 
 from geopy.format import format_degrees
 from geopy.point import Point
 
 
-class TestFormat(unittest.TestCase):
-
-    @unittest.skip("")
-    def test_format(self):
-        """
-        format_degrees
-        """
-        self.assertEqual(
-            format_degrees(Point.parse_degrees('-13', '19', 0)),
-            "-13 19\' 0.0\""
-        )
+@pytest.mark.xfail
+def test_format():
+    assert (
+        format_degrees(Point.parse_degrees('-13', '19', 0)) ==
+        "-13 19\' 0.0\""
+    )
