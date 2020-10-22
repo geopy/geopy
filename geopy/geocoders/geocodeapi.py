d7978426e5ab1322505d42af8e63c78fcce43abc
Dmitrii Kurlov <dmitriik@tutanota.com>
2020-10-22 16:30:32 +0700

    add __init__()

--- geopy/geocoders/geocodeapi.py
+++ geopy/geocoders/geocodeapi.py
@@ -1,5 +1,4 @@
-from geopy.geocoders.base import Geocoder
-
+from geopy.geocoders.base import Geocoder, DEFAULT_SENTINEL
 
 __all__ = ('GeocodeAPI',)
 
@@ -14,3 +13,23 @@ class GeocodeAPI(Geocoder):
     base_api_url = 'https://app.geocodeapi.io/api/v1/'
     geocode_path = 'search'
     reverse_path = 'reverse'
+
+    def __init__(
+        self,
+        api_key,
+        *,
+        timeout=DEFAULT_SENTINEL,
+        proxies=DEFAULT_SENTINEL,
+    ):
+        """
+        :param str api_key: The API key required by GeocodeFarm
+            to perform geocoding requests.
+
+        :param int timeout:
+            See :attr:`geopy.geocoders.options.default_timeout`.
+
+        :param dict proxies:
+            See :attr:`geopy.geocoders.options.default_proxies`.
+        """
+        super.__init__(timeout=timeout, proxies=proxies)
+        self.api_key = api_key
