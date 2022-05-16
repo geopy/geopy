4bc34d33191405d7c5fd399e473a81989f9f5f2c
Kostya Esmukov <kostya@esmukov.ru>
2022-05-16 10:28:21 +0300

    Fix TestGoogleV3::test_geocode_bounds

--- test/geocoders/googlev3.py
+++ test/geocoders/googlev3.py
@@ -259,8 +259,8 @@ class TestGoogleV3(BaseTestGeocoder):
 
     async def test_geocode_bounds(self):
         await self.geocode_run(
-            {"query": "221b Baker St", "bounds": [[50, -2], [55, 2]]},
-            {"latitude": 51.52, "longitude": -0.15},
+            {"query": "Washington", "bounds": [[36.47, -84.72], [43.39, -65.90]]},
+            {"latitude": 38.9071923, "longitude": -77.0368707}
         )
 
     async def test_geocode_bounds_invalid(self):
