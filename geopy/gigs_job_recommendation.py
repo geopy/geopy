ba5d8d34fdeca31ca4fb6ae9c5cdea85bf9ac09b
Aaron Brzowski <aaron.brzowski@gmail.com>
2023-12-09 09:18:12 -0800

    Add unit tests using unittest library

--- geopy/gigs_job_recommendation.py
+++ geopy/gigs_job_recommendation.py
@@ -1,6 +1,8 @@
 import base64
 import requests
+import unittest
 from geopy.geocoders import Nominatim
+from unittest.mock import patch
 
 
 class GigsJobRecommendation:
@@ -23,6 +25,7 @@ class GigsJobRecommendation:
     recommend_jobs(address, radius=8047, min_pay=None, industry=None)
         Returns a dictionary containing the geocoded location and a list of nearby jobs.
     """
+
     def __init__(self, geolocator=None):
         """
         Constructs all the necessary attributes for the GigsJobRecommendation object.
@@ -34,9 +37,7 @@ class GigsJobRecommendation:
             (default is Nominatim with user_agent="gigs_job_recommender")
         """
         self.BASE_URL = "https://getgigs.co/api"
-        self.geolocator = geolocator or Nominatim(
-            user_agent="gigs_job_recommender"
-            )
+        self.geolocator = geolocator or Nominatim(user_agent="gigs_job_recommender")
 
         self.industries = self._get_industries()
 
@@ -65,12 +66,11 @@ class GigsJobRecommendation:
             return None
 
         encoded_crd = self._encode_coordinates(
-            lat=location.latitude,
-            lon=location.longitude
+            lat=location.latitude, lon=location.longitude
         )
 
         jobs = self._get_nearby_jobs(encoded_crd, radius, min_pay, industry)
-        return {'location': location, 'nearby_jobs': jobs}
+        return {"location": location, "jobs": jobs}
 
     def _encode_coordinates(self, lat, lon):
         """
@@ -113,11 +113,11 @@ class GigsJobRecommendation:
             a list of nearby jobs fetched from the Gigs API
         """
         gigs_api_url = f"{self.BASE_URL}/search"
-        params = {'crd': encoded_crd, 'radius': radius}
+        params = {"crd": encoded_crd, "radius": radius}
         if min_pay:
-            params['min_pay'] = min_pay
+            params["min_pay"] = min_pay
         if industry and industry in self.industries:
-            params['industry'] = industry
+            params["industry"] = industry
 
         response = requests.get(gigs_api_url, params=params)
         if response.status_code == 200:
@@ -140,17 +140,17 @@ class GigsJobRecommendation:
             a list of job information dictionaries parsed from the JSON data
         """
         jobs = []
-        for job in json_data.get('data', []):
+        for job in json_data.get("data", []):
             job_info = {
-                'title': job.get('title'),
-                'company': job.get('company', {}).get('name'),
-                'min_pay': job.get('min_pay'),
-                'max_pay': job.get('max_pay'),
-                'location': f"{job.get('city')}, {job.get('state')}",
-                'zipcode': job.get('zipcode'),
-                'posted_date': job.get('posted_date'),
-                'application_url': job.get('application_url'),
-                'requirements': job.get('requirements')
+                "title": job.get("title"),
+                "company": job.get("company", {}).get("name"),
+                "min_pay": job.get("min_pay"),
+                "max_pay": job.get("max_pay"),
+                "location": f"{job.get('city')}, {job.get('state')}",
+                "zipcode": job.get("zipcode"),
+                "posted_date": job.get("posted_date"),
+                "application_url": job.get("application_url"),
+                "requirements": job.get("requirements"),
             }
             jobs.append(job_info)
         return jobs
@@ -167,7 +167,7 @@ class GigsJobRecommendation:
         industries_url = f"{self.BASE_URL}/industries"
         response = requests.get(industries_url)
         if response.status_code == 200:
-            return response.json()['data']
+            return response.json()["data"]
         else:
             return []
 
@@ -176,3 +176,66 @@ class GigsJobRecommendation:
 # recommender = GigsJobRecommendation()
 # result = recommender.recommend_jobs("1600 Amphitheatre Parkway, Mountain View, CA")
 # print(result)
+
+
+class TestGigsJobRecommendation(unittest.TestCase):
+    def setUp(self):
+        self.recommender = GigsJobRecommendation()
+
+    @patch("requests.get")
+    def test__get_industries(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {"data": ["IT", "Healthcare"]}
+        industries = self.recommender._get_industries()
+        self.assertEqual(industries, ["IT", "Healthcare"])
+
+    @patch("requests.get")
+    def test__get_industries_failure(self, mock_get):
+        mock_get.return_value.status_code = 404
+        industries = self.recommender._get_industries()
+        self.assertEqual(industries, [])
+
+    @patch("requests.get")
+    def test_recommend_jobs(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {
+            "data": [
+                {
+                    "title": "Software Engineer",
+                    "company": {"name": "Google"},
+                    "min_pay": 100000,
+                    "max_pay": 150000,
+                    "city": "Mountain View",
+                    "state": "CA",
+                    "zipcode": "94043",
+                    "posted_date": "2023-12-09",
+                    "application_url": "https://google.com",
+                    "requirements": "Python, Java",
+                }
+            ]
+        }
+
+        jobs = self.recommender.recommend_jobs(
+            "1600 Amphitheatre Parkway, Mountain View, CA"
+        )["jobs"]
+
+        self.assertEqual(
+            jobs,
+            [
+                {
+                    "title": "Software Engineer",
+                    "company": "Google",
+                    "min_pay": 100000,
+                    "max_pay": 150000,
+                    "location": "Mountain View, CA",
+                    "zipcode": "94043",
+                    "posted_date": "2023-12-09",
+                    "application_url": "https://google.com",
+                    "requirements": "Python, Java",
+                }
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
