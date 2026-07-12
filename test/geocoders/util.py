geopy/geopy

commit ce21fb70e5d25ee9f6dad5e546cfc2525e1ca779

tests: fix pytest classmethod warning
PytestRemovedIn10Warning: Class-scoped fixture defined as instance method is deprecated.
  Instance attributes set in this fixture will NOT be visible to test methods,
  as each test gets a new instance while the fixture runs only once per class.
  Use @classmethod decorator and set attributes on cls instead.
  See https://docs.pytest.org/en/stable/deprecations.html#class-scoped-fixture-as-instance-method
    fixture_function = resolve_fixture_function(fixturedef, request)

test/geocoders/ignfrance.py
@@ -207,8 +207,9 @@ class TestIGNFranceUsernameAuthProxy(BaseTestGeocoder):
             **kwargs
         )
 
+    @classmethod
     @pytest.fixture(scope='class', autouse=True)
-    async def start_proxy(_, request, class_geocoder):
+    async def start_proxy(cls, request, class_geocoder):
         cls = request.cls
         cls.proxy_server = ProxyServerThread(timeout=cls.proxy_timeout)
         cls.proxy_server.start()

test/geocoders/util.py
@@ -46,8 +46,9 @@ class BaseTestGeocoder(ABC):
     geocoder = None
     delta = 0.5
 
+    @classmethod
     @pytest.fixture(scope='class', autouse=True)
-    async def class_geocoder(_, request, patch_adapter, is_internet_access_allowed):
+    async def class_geocoder(cls, request, patch_adapter, is_internet_access_allowed):
         """Prepare a class-level Geocoder instance."""
         cls = request.cls
         env.is_internet_access_allowed = is_internet_access_allowed

commit 7f176f08cc018a726b795e5ed9f2ab76ab5005a8

tests: fix pytest error in 3.13+ caused by ce21fb7

test/geocoders/ignfrance.py
@@ -2,7 +2,7 @@ import pytest
 
 from geopy.exc import GeocoderQueryError
 from geopy.geocoders import IGNFrance
-from test.geocoders.util import BaseTestGeocoder
+from test.geocoders.util import BaseTestGeocoder, asyncgen_classmethod_pytest91
 from test.proxy_server import ProxyServerThread
 
 
@@ -207,10 +207,9 @@ class TestIGNFranceUsernameAuthProxy(BaseTestGeocoder):
             **kwargs
         )
 
-    @classmethod
     @pytest.fixture(scope='class', autouse=True)
+    @asyncgen_classmethod_pytest91
     async def start_proxy(cls, request, class_geocoder):
-        cls = request.cls
         cls.proxy_server = ProxyServerThread(timeout=cls.proxy_timeout)
         cls.proxy_server.start()
         cls.proxy_url = cls.proxy_server.get_proxy_url()

test/geocoders/util.py
@@ -1,4 +1,5 @@
 import contextlib
+import functools
 import json
 import os
 from abc import ABC, abstractmethod
@@ -38,6 +39,26 @@ class SkipIfMissingEnv(dict):
 env = SkipIfMissingEnv(_env)
 
 
+def asyncgen_classmethod_pytest91(func):
+    # pytest 9.1+ requires a classmethod.
+    # Older pytest doesn't work with it.
+
+    if pytest.version_tuple >= (9, 1):
+        return classmethod(func)
+
+    @functools.wraps(func)
+    async def wrapper(_, request, *args, **kwargs):
+        cls = request.cls
+        gen = func(cls, request, *args, **kwargs)
+        try:
+            item = await gen.__anext__()
+            yield item
+        finally:
+            await gen.aclose()
+
+    return wrapper
+
+
 class BaseTestGeocoder(ABC):
     """
     Base for geocoder-specific test cases.
@@ -46,11 +67,10 @@ class BaseTestGeocoder(ABC):
     geocoder = None
     delta = 0.5
 
-    @classmethod
     @pytest.fixture(scope='class', autouse=True)
+    @asyncgen_classmethod_pytest91
     async def class_geocoder(cls, request, patch_adapter, is_internet_access_allowed):
         """Prepare a class-level Geocoder instance."""
-        cls = request.cls
         env.is_internet_access_allowed = is_internet_access_allowed
 
         geocoder = cls.make_geocoder()